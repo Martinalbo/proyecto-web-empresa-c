610bd0d6eed6aa37cd26bb9e3f299b7b9dd47042
Martin <Albornoz_Martin1996@hotmail.com>
2022-10-19 18:41:24 -0300

    nuevocommit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function clasificarValor(valor) {
+    if (valor < 5) {
+        console.log("Es menor que 5.");
+    } else if (valor < 10) {
+        console.log("Es menor que 10.");
+    } else {
+        console.log("Mayor o igual a 10");
+    }
+}
+
+clasificarValor(8); 
